75981c22b6610448f7487d1be3bf0ebe87bfafdd
Hadrien lucas <hadrienl@theodo.fr>
2020-06-02 17:36:09 +0200

    Permissions to create only for anyone

--- training_api/users/views.py
+++ training_api/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
 
@@ -9,3 +10,17 @@ from rest_framework import viewsets
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_permissions(self):
+        need_authentication = [
+            "retrieve",
+            "list",
+            "update",
+            "destroy",
+            "partial_update",
+        ]
+        if self.action in need_authentication:
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
