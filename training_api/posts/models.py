eb7f9a2375ca4b95c19841fe5d43aea713ebedd1
Hadrien lucas <hadrienl@theodo.fr>
2020-06-02 16:13:24 +0200

    Added DateTime to posts

--- training_api/posts/models.py
+++ training_api/posts/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=150)
     content = models.CharField(max_length=256)
     user_id = models.IntegerField()
+    date = models.DateTimeField()
 
     def __str__(self):
         return self.title
