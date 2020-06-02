e293d69ba5ec9a9e31ccc450d42d274fcb768ac6
Hadrien lucas <hadrienl@theodo.fr>
2020-06-02 12:38:15 +0200

    Added authentication with JWT for all routes

--- training_api/training_api/urls.py
+++ training_api/training_api/urls.py
@@ -15,5 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token
 
-urlpatterns = [path("admin/", admin.site.urls), path("posts/", include("posts.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("posts/", include("posts.urls")),
+    path("token/", obtain_jwt_token),
+]
