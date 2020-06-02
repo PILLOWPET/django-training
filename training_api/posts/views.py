67cbf54b57572923efa2b4dad566787a025663b3
Hadrien lucas <hadrienl@theodo.fr>
2020-06-02 16:39:59 +0200

    More elegant permission classes

--- training_api/posts/views.py
+++ training_api/posts/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from .models import Post
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .serializers import PostSerializer
 
 # Create your views here.
@@ -9,11 +9,4 @@ from .serializers import PostSerializer
 class PostAPIView(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
-    def get_permissions(self):
-        need_authentication = ["update", "create", "destroy", "partial_update"]
-        if self.action in need_authentication:
-            permission_classes = [IsAuthenticated]
-        else:
-            permission_classes = [AllowAny]
-        return [permission() for permission in permission_classes]
+    permission_classes = [IsAuthenticatedOrReadOnly]
