84d73742c065fcff9880b5095ef2b067f0ba6355
Hadrien lucas <hadrienl@theodo.fr>
2020-06-02 19:52:29 +0200

    Auto datetime on field

--- training_api/posts/models.py
+++ training_api/posts/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=150)
     content = models.CharField(max_length=256)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
