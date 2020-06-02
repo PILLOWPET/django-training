65f5c29916c5209b65807d71f83c30fdf7d5733e
Hadrien lucas <hadrienl@theodo.fr>
2020-06-02 13:29:52 +0200

    Allow update, create or delete only if connected

--- training_api/posts/views.py
+++ training_api/posts/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from .models import Post
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import PostSerializer
 
 # Create your views here.
@@ -8,3 +9,12 @@ from .serializers import PostSerializer
 class PostAPIView(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def get_permissions(self):
+        need_authentication = ["update", "create", "destroy", "partial_update"]
+        if self.action in need_authentication:
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [AllowAny]
+        print(permission_classes)
+        return [permission() for permission in permission_classes]
