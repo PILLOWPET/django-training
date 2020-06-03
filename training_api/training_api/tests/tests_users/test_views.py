ae132f48ccf7b15c5174aaf640a17601509e4959
Hadrien lucas <hadrienl@theodo.fr>
2020-06-03 16:11:24 +0200

    Test views of users

---
+++ training_api/training_api/tests/tests_users/test_views.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from django.contrib.auth.models import User
+
+
+class UserViewSetTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        test_user_1 = User.objects.create_user(
+            username="test_user", password="test_password"
+        )
+        test_user_1.save()
+
+    def test_logged_in(self):
+        url = "/token/"
+        resp = self.client.post(
+            url, {"username": "test_user", "password": "test_password"}
+        )
+        self.assertTrue(resp.status_code, 200)
+        self.assertTrue("token" in resp.data)
+
+    def test_create(self):
+        url = "/users/"
+        resp = self.client.post(
+            url, {"username": "test_user2", "password": "test_password2"}
+        )
+        self.assertTrue(resp.status_code, 200)
+        self.assertTrue("username" in resp.data)
