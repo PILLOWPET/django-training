c6321c685518810f2f2c59a06868953d2a3e087d
Hadrien lucas <hadrienl@theodo.fr>
2020-06-03 17:56:24 +0200

    add a profile to follow route

--- training_api/profiles/urls.py
+++ training_api/profiles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import ProfileAPIView
+from .views import ProfileAPIView, FollowAPIView
 from profiles import views
 
 router = routers.DefaultRouter()
+router.register(r"follow", views.FollowAPIView)
 router.register(r"", views.ProfileAPIView)
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = router.urls
--- training_api/profiles/views.py
+++ training_api/profiles/views.py
@@ -1,8 +1,10 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from .models import Profile
 from rest_framework.permissions import IsAuthenticated, AllowAny, SAFE_METHODS
 from .serializers import ProfileSerializer
 from .permissions import ownProfile, IsValidAction
+from rest_framework.response import Response
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
@@ -17,3 +19,31 @@ class ProfileAPIView(viewsets.ModelViewSet):
         else:
             permission_classes = [ownProfile & IsValidAction]
         return [permission() for permission in permission_classes]
+
+
+class FollowAPIView(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action == "create":
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [~AllowAny]
+        return [permission() for permission in permission_classes]
+
+    def create(self, request):
+        try:
+            profile_id = request.data["id"]
+        except KeyError:
+            return Response("Id not specified", status=status.HTTP_400_BAD_REQUEST)
+        try:
+            profile_to_follow = Profile.objects.get(pk=profile_id)
+        except ObjectDoesNotExist:
+            return Response("Invalid profile id", status=status.HTTP_400_BAD_REQUEST)
+        user_profile = request.user.profile
+        user_profile.following_profiles.add(profile_to_follow)
+        serializer = ProfileSerializer(user_profile, context={"request": request})
+        user_profile.save()
+        return Response(serializer.data)
