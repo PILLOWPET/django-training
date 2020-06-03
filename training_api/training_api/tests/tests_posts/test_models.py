50b035eca3667cec7985006d43d198564ad29b83
Hadrien lucas <hadrienl@theodo.fr>
2020-06-03 16:26:07 +0200

    Test posts

---
+++ training_api/training_api/tests/tests_posts/test_models.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from posts.models import Post
+
+
+class UserModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create_user(username="test_user", password="test_password")
+        Post.objects.create(title="test_title", content="test_content", user=user)
+
+    def test_post_creation(self):
+        post = Post.objects.get(id=1)
+        self.assertEquals(str(post), "test_title")
+        self.assertIsNotNone(post.date)
