e308bfebd0106d3dcf6a121949674d3cc3140e6d
Hadrien lucas <hadrienl@theodo.fr>
2020-06-04 12:31:06 +0200

    Include date in post & comment

--- training_api/posts/serializers.py
+++ training_api/posts/serializers.py
@@ -8,10 +8,10 @@ from users.serializers import UserSerializer
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ("id", "title", "content", "user")
+        fields = ("id", "title", "content", "user", "date")
 
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ("id", "post", "content", "user")
+        fields = ("id", "post", "content", "user", "date")
