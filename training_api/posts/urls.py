e268c167fe15c4c348961382d7d988ea53a072d7
Hadrien lucas <hadrienl@theodo.fr>
2020-06-04 11:05:23 +0200

    Route for followed posts

--- training_api/posts/urls.py
+++ training_api/posts/urls.py
@@ -3,6 +3,8 @@ from rest_framework import routers
 from posts import views
 
 router = routers.DefaultRouter()
+router.register(r"followed", views.PostFollowedAPIView)
 router.register(r"", views.PostAPIView)
+
 
 urlpatterns = [path("", include(router.urls))]
