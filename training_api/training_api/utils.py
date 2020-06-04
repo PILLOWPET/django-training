bf82bc8a3c696be1eea876fc3fc900d865671239
Hadrien lucas <hadrienl@theodo.fr>
2020-06-04 16:56:03 +0200

    removed print

--- training_api/training_api/utils.py
+++ training_api/training_api/utils.py
@@ -3,13 +3,11 @@ from rest_framework.views import exception_handler
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
-    print(response)
     if response is not None:
         try:
             message = response.data["detail"]
-            print(message)
             response.data["status_code"] = response.status_code
             response.data["detail"] = message
         except KeyError:
-            print(response.data)
+            pass
     return response
