d26e9d90a4238dc75a49ec1572f29557463c0454
Hadrien lucas <hadrienl@theodo.fr>
2020-06-04 12:34:11 +0200

    Added comment testing

--- training_api/training_api/tests/tests_posts/test_models.py
+++ training_api/training_api/tests/tests_posts/test_models.py
@@ -1,15 +1,23 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from posts.models import Post
+from posts.models import Post, Comment
 
 
 class PostModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         user = User.objects.create_user(username="test_user", password="test_password")
-        Post.objects.create(title="test_title", content="test_content", user=user)
+        post = Post.objects.create(
+            title="test_title", content="test_content", user=user
+        )
+        Comment.objects.create(user=user, post=post, content="test_content")
 
     def test_post_creation(self):
         post = Post.objects.get(id=1)
-        self.assertEquals(str(post), "test_title")
+        self.assertEqual(str(post), "test_title")
         self.assertIsNotNone(post.date)
+
+    def test_comment_creation(self):
+        comment = Comment.objects.get(id=1)
+        self.assertEqual(str(comment), "test_content")
+        self.assertIsNotNone(comment.date)
--- training_api/training_api/tests/tests_posts/test_views.py
+++ training_api/training_api/tests/tests_posts/test_views.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 
 from django.contrib.auth.models import User
-from posts.models import Post
+from posts.models import Post, Comment
 
 
 class PostViewSetTest(TestCase):
@@ -12,19 +12,31 @@ class PostViewSetTest(TestCase):
             username="test_user", password="test_password"
         )
         test_user_1.save()
-        Post.objects.create(
+        post_1 = Post.objects.create(
             title="test_title", content="test_content", user=test_user_1
         )
+        post_1.save()
+        comment_1 = Comment.objects.create(
+            content="test", user=test_user_1, post=post_1
+        )
+        comment_1.save()
 
     def test_retrieve(self):
         url = "/posts/1/"
         resp = self.client.get(url)
-        self.assertTrue(resp.status_code, 200)
+        self.assertEqual(resp.status_code, 200)
         self.assertTrue("title" in resp.data)
 
     def test_list(self):
         url = "/posts/"
         resp = self.client.get(url)
-        self.assertTrue(resp.status_code, 200)
+        self.assertEqual(resp.status_code, 200)
         self.assertGreater(len(resp.data), 0)
         self.assertTrue("title" in resp.data[0])
+
+    def test_comment(self):
+        url = "/posts/1/comments/"
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertGreater(len(resp.data), 0)
+        self.assertTrue("content" in resp.data[0])
