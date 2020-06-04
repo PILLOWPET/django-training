e90c43b1865081a9af7680a9426846e3615e6e78
Hadrien lucas <hadrienl@theodo.fr>
2020-06-04 15:27:31 +0200

    unused imports

--- training_api/posts/serializers.py
+++ training_api/posts/serializers.py
@@ -2,8 +2,6 @@ from rest_framework import serializers
 
 from .models import Post, Comment
 
-from users.serializers import UserSerializer
-
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
