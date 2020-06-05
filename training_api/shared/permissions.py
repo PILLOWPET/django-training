be0d9083931769c5040a65a56e179bfaba4a1554
Hadrien lucas <hadrienl@theodo.fr>
2020-06-05 14:56:02 +0200

    removed print

--- training_api/shared/permissions.py
+++ training_api/shared/permissions.py
@@ -3,10 +3,7 @@ from rest_framework.permissions import BasePermission
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        print(request.user)
-        print(request.user.is_staff)
         return request.user.is_staff
 
     def has_object_permission(self, request, view, obj):
-        print(request.user)
         return request.user.is_staff
